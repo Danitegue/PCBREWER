f8d1bdafd51bb0cbe2b88ee7f6fd17b4f974a3dd
danitegue <daniel.santana@luftblick.at>
2022-01-28 22:13:51 +0000

    Adjusted the windows launchers for the new brw_functions

--- Windows/Launcher_B185_pcbasic.bat
+++ Windows/Launcher_B185_pcbasic.bat
@@ -1,55 +1,81 @@
 @echo off
 title %~nx0
+
 rem *********************************************************************************************
-rem Example of Launcher for running the Brewer Software into PCBASIC, for brewer instrument number 185, in online mode.
+rem Example of Windows launcher for running the Brewer Software into PCBASIC,
+rem for brewer instrument number 185, in online mode.
 rem *********************************************************************************************
 
-rem PCBASIC_PATH is the path in which the main.py file of pcbasic is located (if installed with pip, it should be in C:\Python27\Lib\site-packages\pcbasic)
+rem PTYHON_EXEC is the path to the python executable file (usually in C:\Python27\python.exe)
+set PTYHON_EXEC=C:\Python27\python.exe
+
+rem PCBASIC_PATH is the folder in which the main.py file of pcbasic is located
+rem (if installed with pip, it should be in C:\Python27\Lib\site-packages\pcbasic)
 set PCBASIC_PATH=C:\Python27\Lib\site-packages\pcbasic
 
-rem PYTHON_DIR is the folder in which the python.exe is located (usually in C:\Python27)
-set PYTHON_DIR=C:\Python27
-
-rem Folder to mount as unit C: (For Brewer soft, C: must be C: Otherwise SHELL commands of the main.asc won't work.)
+rem Folder to mount as unit C:. (empty means none):
+rem (for windows it will be easier if you mount C:\ as C:\)
 set MOUNT_C=C:\
 
-rem Folder to mount as unit D: (For Brewer soft, D: must be D: Otherwise SHELL commands won't work.)(Empty if not needed)
+rem Folder to mount as unit D:. (empty means none).
 set MOUNT_D=
 
 rem Set the name of the BASIC program to run (For brewer soft, main.asc)
 set PROGRAM=main.asc
 
-rem Set here the com ports binding: Which phisical COM port is going to be binded to each PCBASIC virtual com port.
-rem for example "COM_PORT_1=PORT:COM14" will bind the real COM14 port of the pc, to the PCBASIC com1.
-rem One can use "COM_PORT_2=stdio:" for binding a dummy port into the PCBASIC com2.
+rem PROGRAM_PATH: folder where the BASIC program to be loaded is located. (=Where is main.asc?)
+set PROGRAM_PATH=C:\brw#185\Prog410
+
+rem Set here the com ports binding: Which physical COM port is going to be binded to each PCBASIC virtual com port.
+rem for example COM_PORT_1=PORT:COM14 will bind the real COM14 port of the pc, to the PCBASIC com1.
+rem One can use COM_PORT_2=stdio: for binding a dummy port (no port) into the PCBASIC com2.
 rem Note: the brewer software must be configured to talk with the corresponding virtual port.
-rem Note: for brewer soft v375, it is needed to have at least a dummy port set, even running in nobrew mode.
+rem Note: for brewer soft v375, it is needed to have at least a dummy port set, even when running in nobrew mode.
 set COM_PORT_1=PORT:COM14
 set COM_PORT_2=stdio:
 
-rem Set the directory in which is going to be saved the PCBASIC session log file. This directory must exist, otherwise PCBASIC will crash.
-set LOG_DIR=C:\Temp
+rem PCBASIC_LOG_DIR: directory where the pcbasic session log file is going to be written.
+rem Note: Ensure that this directory exist and is writeable. Otherwise pcbasic will crash.
+set PCBASIC_LOG_DIR=C:\Temp
 
-rem Brewer instrument ID: for example ID=185. This is for having an identifier in the filename of the PCBASIC session log file 
+rem DEBUG_MODE mode: If True, more info will be written in the pcbasic session log file, for example
+rem the files that the gw code is opening, the COM port communications, and the result of the shell commands
+rem executed by the Brw_function script. It is recommended to set this to True the first days, and once you are sure
+rem that everything works fine (=no warnings in the log file), you could change it to False for regular operation.
+set DEBUG_MODE=True
+
+rem Brewer instrument ID: for example ID=185.
+rem This is just for having an identifier in the filename of the pcbasic session log file
 set ID=185
 
-rem BRWFUNCT_DIR is the folder in which the Brw_functions.py is located:
-set BRWFUNCT_DIR=C:\PCBREWER
+rem BRWFUNCT_PATH: is the file path of the Brw_functions.py file.
+rem It is needed to execute the SHELL commands of the brewer software through python code.
+set BRWFUNCT_PATH=C:\PCBREWER\Brw_functions.py
 
-rem ---------NEEDED ENVIROMENT VARIABLES FOR BREWER PROGRAM: BREWDIR AND NOBREW:----------
+rem BRWFUNCT_LOG_DIR is the folder where the Brw_functions.py log file is going to be written.
+rem This log file is helpful to check if the SHELL commands are working fine or not.
+set BREWFUNCT_LOG_DIR=C:\Temp
 
-rem Set the BREWDIR enviroment variable: where to find the main.asc respect the pcbasic mounted drives (full path)
+rem ---------NEEDED ENVIRONMENT VARIABLES FOR BREWER PROGRAM: BREWDIR AND NOBREW:----------
+
+rem Set the BREWDIR environment variable: where to find the main.asc, with respect the pcbasic mounted drives!!! (full path)
+rem Example1: If your main.asc file is in C:\brw#185\Prog410, and you mounted C:\ as the pcbasic unit C:\ (MOUNT_C=C:\),
+rem then BREWDIR=C:\brw#185\Prog410 (will be the same as PROGRAM_PATH)
+rem Example2: If your main.asc file is in C:\test\brw#185\Prog410, and you mounted the unit C:\test as the pcbasic unit C:\ (MOUNT_C=C:\test),
+rem then BREWDIR=C:\brw#185\Prog410. (in this case PROGRAM_PATH will be different)
 set BREWDIR=C:\brw#185\Prog410
 
-rem Set the NOBREW enviroment variable: If NOBREW=1 the brewer program will run in offline mode (No COM port communications). Empty = online mode.
+rem Set the NOBREW environment variable:
+rem If NOBREW=1 the brewer program will run in offline mode (No COM port communications). Empty = online mode.
 set NOBREW=
 
-rem Set debug mode: It should be False for regular use. It can be set to True eventually to see in the pcbasic session log file (see LOG_DIR)
-rem what files is the gw code oppening, or to see if the COM port communications are working properly, or to see if the Brw_functions shell commands are working as expected.
-set DEBUG_MODE=False
 
-rem Remember to have configured accordingly the OP_ST.FIL and OP_ST.III files of the brewer software.
 
+
+rem Remember to have configured accordingly the brw#NNN/ProgVVV/OP_ST.FIL and brw#NNN/bdataNNN/NNN/OP_ST.NNN files of the brewer software.
+rem the paths in the second line of these files must be the paths to the bdata folder, with respect the pcbasic mounted drives!!!
+rem Example: if your bdata folder is in C:\brw#072\bdata072, and you mounted C:\ as the pcbasic unit C:\ (MOUNT_C=C:\),
+rem then the line 2 of the OP_ST.FIL and OP_ST.072 has to be C:\brw#072\bdata072\
 
 
 
@@ -62,12 +88,13 @@ rem save the current dir, to restore on exit
 set CURR_DIR=%CD%
 
 rem add to the pythonpath the pcbasic dir, for being able to look for the needed libraries.
-set PYTHONPATH=%PYTHON_DIR%;%PCBASIC_PATH%
+set PYTHONPATH=%PTYHON_EXEC%;%PCBASIC_PATH%
 
 rem get the isodate to write it into the pcbasic log filename (local pc hour)
-for /f "delims=" %%a in ('powershell get-date -format "{yyyyMMddTHHmmssZ}"') do set isodate=%%a
+for /f "delims=" %%a in ('powershell get-date -format "{yyyyMMddTHHmmssZ}"') do set ISODATE=%%a
+
 rem Change the current path to the Brewer program directory to ensure correct operation (full path)
-cd %BREWDIR%
+cd %PROGRAM_PATH%
 
 rem Change the prompt as a reminder that the Brewer software is running
 PROMPT Brewer $P$G
@@ -75,14 +102,14 @@ PROMPT Brewer $P$G
 @echo on
 
 rem set the date in the OP_ST file before launching the software:
-%PYTHON_DIR%\python.exe %BRWFUNCT_DIR%\Brw_functions.py setdate
+%PTYHON_EXEC% %BRWFUNCT_DIR% setdate
 
 rem * Run the Brewer software with PCBASIC
-%PYTHON_DIR%\python.exe -m pcbasic --interface=graphical --mount=Z:.,C:%MOUNT_C%,D:%MOUNT_D% --current-device=Z --com1=%COM_PORT_1% --com2=%COM_PORT_2% --run=%PROGRAM% --quit=False -f=10 --shell="%PYTHON_DIR%\python.exe %BRWFUNCT_DIR%\Brw_functions.py" --debug=%DEBUG_MODE% --logfile=%LOG_DIR%\pcbasic_brewer_log_%ID%_%isodate%.txt
+%PTYHON_EXEC% -m pcbasic --interface=graphical --mount=Z:.,C:%MOUNT_C%,D:%MOUNT_D% --current-device=Z --com1=%COM_PORT_1% --com2=%COM_PORT_2% --run=%PROGRAM% --quit=False -f=10 --shell="%PTYHON_EXEC% %BRWFUNCT_PATH%" --debug=%DEBUG_MODE% --logfile=%PCBASIC_LOG_DIR%\pcbasic_brewer_log_%ID%_%ISODATE%.txt
 
 
 rem * On exit, undo the changes what were done above
 PROMPT $P$G
 rem restore the current dir
 cd %CURR_DIR%
-ECHO "Have a nice day!"
+echo "PCBASIC closed, have a nice day!"
--- Windows/Launcher_brewer_simulator.bat
+++ Windows/Launcher_brewer_simulator.bat
@@ -1,12 +1,12 @@
 @echo on
 title Brw_simulator
 
-rem PYTHON_DIR is the folder in which the python.exe is located (usually in C:\Python27)
-set PYTHON_DIR=C:\Python27
+rem PYTHON_EXEC is the python executable file path (usually in C:\Python27\python.exe)
+set PYTHON_EXEC=C:\Python27\python.exe
 
 rem set the path accordingly to where the Brw_simulator.py file is located.
-set BREWSIM_DIR=C:\PCBREWER
+set BREWSIM_DIR=C:\PCBREWER\Brw_simulator.py
 
-rem running Brw_simulator.py
-%PYTHON_DIR%\python.exe %BREWSIM_DIR%\Brw_simulator.py
+rem Launch program
+%PYTHON_EXEC% %BREWSIM_DIR%
 pause
